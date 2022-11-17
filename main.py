df40e0204d8f2ff57aa3106dd4c6ff00560e1261
seymahll <seymhll@gmail.com>
2022-11-17 10:20:41 +0300

    vs2

---
+++ main.py
@@ -0,0 +1,42 @@
+print("Merhaba Etiya")
+# string : metinler  "" kullanılır
+text = 'hilal'
+print(text)
+# integer : tam sayı "" kullanılmaz
+number = 5
+print(number)
+# float : ondalıklı sayı 
+dnumber = 5.3
+print(dnumber)
+# boolean : True , False
+# matematiksel operatörler
+print(number + 5)
+print(number - 5)
+print(number * 5)
+print(number / 5)
+print(number % 5) #mod bölümde kalanı verir
+# mantıksal operatörler
+print(1==2)
+print(1!=2)
+print(1>2)
+print(1<2)
+print(1<=2)
+print(1>=2)
+print(10%2==0)
+print(50/2==25)
+print(50/2==25.0)
+# string fonksiyonları
+text = 'Merhaba Etiya'
+print(text.upper()) # Metni byk yazdır
+print(text.lower()) # Metni kçk yazdır
+print(text.startswith("Met")) # Cümle başı ... ile başlıyor mu?
+print(text.endswith("Etiya")) # Cümle sonu ... ile bitiyor mu?
+# f string
+name = 'Hilal'
+age = 27
+company = 'Etiya'
+print(f"{name} {age} yasinda ve {company}'da calisiyor")
+name = 'Hilal'
+age = "27"
+company = 'Etiya' 
+print(name + age + company)
