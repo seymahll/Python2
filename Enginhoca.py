8418812c4bc3959b550711969d655606608de787
seymahll <seymhll@gmail.com>
2022-11-17 11:51:19 +0300

    Eders1

---
+++ Enginhoca.py
@@ -0,0 +1,18 @@
+baslik = 'haberiniz olsun'
+vade = 12
+faizOrani1 =1.47
+faizOrani2 =1.44
+print(baslik)
+print(type(baslik))
+print(type(vade))
+print(type(faizOrani1))
+mesaj = "Hoşgeldin"
+müsteriAdi = "Hilal"
+müsteriSoyadi = "Özkir"
+print(mesaj+müsteriAdi+müsteriSoyadi) 
+print(mesaj+ " " + müsteriAdi+ " " + müsteriSoyadi) 
+sonuc = mesaj+ " " + müsteriAdi+ " " + müsteriSoyadi
+print(sonuc)
+x=10
+y=20
+print(x+y)
